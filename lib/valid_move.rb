4d42179370782cf4dcb540abe3302cc0ff36cf6d
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-17 00:29:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,5 @@
 <<<<<<< HEAD
 def valid_move?(board, index)
-=======
-def valid_move(board, index)
->>>>>>> 9fef1f8b63c70c157716ff2c9d7f01f6809d979d
    if (index.between?(0, 8)) && (position_taken?(board, index) == false)
  return true
 else
